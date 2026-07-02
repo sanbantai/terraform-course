1e9c84a28176e34b4e0e89c626439660007f3d16
Ilya Gruzman <gruzmanworks@gmail.com>
2026-07-02 17:00:04 +0300

    last one

--- ex11-public-modules/networking.tf
+++ ex11-public-modules/networking.tf
@@ -1,3 +1,9 @@
+locals {
+  vpc_cidr            = "10.0.0.0/16"
+  private_subnet_cidr = ["10.0.1.0/24"]
+  public_subnet_cidr  = ["10.0.128.0/24"]
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
@@ -5,10 +11,12 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.6.1"
-  azs = data.aws_availability_zones.available.names
 
-  cidr = "10.0.0.0/16"
-  name = "11-public-modules"
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.128.0/24"]
+  azs             = data.aws_availability_zones.available.names
+  cidr            = local.vpc_cidr
+  name            = local.project_name
+  private_subnets = local.private_subnet_cidr
+  public_subnets  = local.public_subnet_cidr
+
+  tags = local.shared_tags
 }
